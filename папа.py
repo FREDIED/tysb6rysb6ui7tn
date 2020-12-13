c9ae528373e1014a0ab3af296a00f592d2e1be94
FREDIED <64380397+FREDIED@users.noreply.github.com>
2020-12-13 16:46:12 +0300

    Delete папа.py

--- папа.py
+++
@@ -1,9 +0,0 @@
-q = (int(input("?\n")))
-w = (input('Только четные?\n'))
-if w == "y":
-    for qq in range(q):
-        if qq % 2 == 0:
-            print(qq)
-if w == "n":
-    for qq in range(q):
-            print(qq+1)
